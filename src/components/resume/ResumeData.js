f33716cd2ab3243c776d97e407f9e163dbe197bd
Boldi247 <kovacs.boldizsar0321@gmail.com>
2024-02-26 22:22:21 +0100

    update resume data

---
+++ src/components/resume/ResumeData.js
@@ -0,0 +1,39 @@
+const experiences = [
+  {
+    id: 1,
+    category: "education",
+    icon: "icon-book-open",
+    year: "2023 - present (expected: 2025)",
+    title: "Computer Science MSc",
+    description: "University of Debrecen, Hungary - Faculty of Informatics.",
+  },
+  {
+    id: 2,
+    category: "education",
+    icon: "icon-book-open",
+    year: "2020 - 2023",
+    title: "Computer Science BSc",
+    description:
+      "University of Debrecen, Hungary - Faculty of Informatics. Graduated with a degree of mark 5 (out of 5).",
+  },
+  {
+    id: 3,
+    category: "work",
+    icon: "icon-briefcase",
+    year: "2023.10 - present",
+    title: "Jabil Circuit Hungary Ltd.",
+    description:
+      "Front-end developer position. React.js, TypeScript, Cypress, Mendix, Azure DevOps, Git and other technologies.",
+  },
+  {
+    id: 4,
+    category: "work",
+    icon: "icon-briefcase",
+    year: "2022.06 - 2022.09",
+    title: "Muncipal Government of Hangony",
+    description:
+      "Practice of Profession (CS BSc) - renovation of the town's website and solving IT infrastructural problems.",
+  },
+];
+
+export default experiences;
